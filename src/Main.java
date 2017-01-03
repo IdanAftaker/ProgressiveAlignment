e4b7101eb71e71b18f46637ed18aba2988633ee7
idanaftaker <idanaftaker@gmail.com>
2017-01-03 22:11:22 +0200

    add buttons and panels

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,16 @@ public class Main implements ActionListener {
     //===Members===
     private JFrame mainFrame;
     private JPanel mainPanel;
+    private JPanel upperPanel;
+    private JPanel buttonsPanel;
+    private JPanel lowerPanel;
+    private JButton btn_AddSequence;
+    private JButton btn_Compute;
+    private JButton btn_SubMatrix;
+
+    private JLabel lblEnterSequences;
+    private JTextField textField;
+
 //    private JButton btn_addItem;
 //    private JTextField textField;
 //    private String[] Seqs = new String[10000];
@@ -27,15 +37,64 @@ public class Main implements ActionListener {
         this.mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.mainFrame.setVisible(true);
 
+        //Labels
+        this.lblEnterSequences = new JLabel("Enter Sequences :");
+
+
+        //Add buttons
+        //Add Sequence
+        this.btn_AddSequence = new JButton("Add Sequence");
+        btn_AddSequence.setFont(new Font("Helvetica",Font.ITALIC,14));
+        btn_AddSequence.addActionListener(this);
+
+        //Compute
+        this.btn_Compute = new JButton("Compute");
+        btn_Compute.setFont(new Font("Helvetica",Font.ITALIC,14));
+        btn_Compute.addActionListener(this);
+
+        //Subtitution matrix
+        this.btn_SubMatrix = new JButton("Subtitution Matrix File:");
+        btn_SubMatrix.setFont(new Font("Helvetica",Font.ITALIC,14));
+        btn_SubMatrix.addActionListener(this);
+
+
+
+        //Buttons Panel
+        this.buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new GridLayout());
+        buttonsPanel.add(btn_AddSequence);
+        buttonsPanel.add(btn_SubMatrix);
+        buttonsPanel.add(btn_Compute);
+
+
+
+        //Upper panel
+        this.upperPanel = new JPanel();
+        upperPanel.setLayout(new BorderLayout());
+        upperPanel.add(lblEnterSequences, border.NORTH);
+        upperPanel.add(buttonsPanel, border.SOUTH);
+
         //Main panel of the screen
         this.mainPanel = new JPanel();
-        this.mainPanel.setBackground(Color.white);
-        this.mainFrame.add(this.mainPanel, border.CENTER);
+        mainPanel.setBackground(Color.white);
+        mainFrame.add(this.mainPanel, border.CENTER);
+        mainFrame.add(upperPanel,border.NORTH);
+
+
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btn_AddSequence){
+            // TODO: 03/01/2017
+        }
+        if (e.getSource() == btn_Compute){
+            // TODO: 03/01/2017
+        }
+        if (e.getSource() == btn_SubMatrix){
+            // TODO: 03/01/2017
+        }
 
     }
 
